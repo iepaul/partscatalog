824643fb8c58c7216082725b1ee7f34ba1a47d98
iepaul <paul.hartigan@gmail.com>
2014-08-05 15:31:57 +0100

    add camera functionality

--- PartsCatalog/src/ie/sal/partscatalog/MainActivity.java
+++ PartsCatalog/src/ie/sal/partscatalog/MainActivity.java
@@ -1,13 +1,24 @@
 package ie.sal.partscatalog;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +31,20 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	private Button scanBtn;
 	private TextView formatTxt, contentTxt;
 	
+	// Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int SCANNER_CODE = 0x0000c0de;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    
+    // directory name to store captured images and videos
+    private static final String IMAGE_DIRECTORY_NAME = "Parts Catalog";
+ 
+    private Uri fileUri; // file url to store image/video
+ 
+    private ImageView imgPreview;
+    private Button btnCapturePicture;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +55,10 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		contentTxt = (TextView)findViewById(R.id.scan_content);
 		scanBtn.setOnClickListener(this);
 		
+		imgPreview = (ImageView) findViewById(R.id.imgPreview);
+        btnCapturePicture = (Button) findViewById(R.id.btnCapturePicture);
+        btnCapturePicture.setOnClickListener(this);
+        
     }
 
 
@@ -61,9 +90,18 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
 			scanIntegrator.initiateScan();
 			}
+		else if(v.getId()==R.id.btnCapturePicture){
+			//take picture
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+		    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		    // start the image capture Intent
+		    startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+		}
 	}
 	
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == SCANNER_CODE){
 		//retrieve scan result
 		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 		if (scanningResult != null) {
@@ -80,5 +118,84 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		    toast.show();
 		}
 		}
+		if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // successfully captured the image
+	            // display it in image view
+	            previewCapturedImage();
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // user cancelled Image capture
+	            Toast.makeText(getApplicationContext(),
+	                    "User cancelled image capture", Toast.LENGTH_SHORT)
+	                    .show();
+	        } else {
+	            // failed to capture image
+	            Toast.makeText(getApplicationContext(),
+	                    "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+	                    .show();
+	        }
+	    }
+		}
+	
+	private void previewCapturedImage() {
+        try {
+            // hide video preview
+            imgPreview.setVisibility(View.VISIBLE);
+ 
+            // bimatp factory
+            BitmapFactory.Options options = new BitmapFactory.Options();
+ 
+            // downsizing image as it throws OutOfMemory Exception for larger
+            // images
+            options.inSampleSize = 8;
+ 
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+ 
+            imgPreview.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type){
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "MyCameraApp");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("MyCameraApp", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "IMG_"+ timeStamp + ".jpg");
+	    } else if(type == MEDIA_TYPE_VIDEO) {
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "VID_"+ timeStamp + ".mp4");
+	    } else {
+	        return null;
+	    }
+
+	    return mediaFile;
+	}
 	
 }
